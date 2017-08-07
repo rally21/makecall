60cf93d77a63c49db78f0782f98ddabaef559a4a
Thomas Becker <rally21@Thomass-MacBook-Pro.local>
2017-08-07 19:34:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rally21/makecall5/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.rally21.makecall5;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Button buttonCall = (Button) findViewById(R.id.buttonCall);
+//        buttonCall.setOnClickListener(new View.OnClickListener() {
+
+        button = (Button) findViewById(R.id.buttonCall);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:4046805737"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+    }
+}
